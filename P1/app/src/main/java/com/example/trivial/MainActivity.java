6f91b6a2b13951d919935252b44e049bf93f5b90
scuadrosf <101071401+scuadrosf@users.noreply.github.com>
2023-10-03 21:01:47 +0200

    radiogroup

--- P1/app/src/main/java/com/example/trivial/MainActivity.java
+++ P1/app/src/main/java/com/example/trivial/MainActivity.java
@@ -3,8 +3,8 @@ package com.example.trivial;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,8 +14,8 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private TextView questionTextView;
-    private RadioButton option1RadioButton, option2RadioButton, option3RadioButton, option4RadioButton;
-    private Button nextButton;
+    private RadioGroup radioGroup;
+    private Button nextButton, againButton;
     private ArrayList<Question> questions;
     private int currentQuestionIndex = 0;
     private int score = 0;
@@ -24,23 +24,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        radioGroup = findViewById(R.id.radioGroup);
         questionTextView = findViewById(R.id.questionTextView);
-        option1RadioButton = findViewById(R.id.option1RadioButton);
-        option2RadioButton = findViewById(R.id.option2RadioButton);
-        option3RadioButton = findViewById(R.id.option3RadioButton);
-        option4RadioButton = findViewById(R.id.option4RadioButton);
         nextButton = findViewById(R.id.nextButton);
+        againButton = findViewById(R.id.againButton);
 
         // Inicializar vistas y preguntas aquí (usando findViewById)
 
         // Cargar preguntas
         questions = new ArrayList<>();
-        questions.add(new Question("¿Ejemplo de pregunta?", new String[]{"Opción 1", "Opción 2", "Opción 3", "Opción 4"}, 0));
-        questions.add(new Question("¿Pregunta 2?", new String[]{"Adrian", "Marica", "Si", "Tambien"}, 2));
+        questions.add(new Question("¿Cuál es el país de origen del fútbol?", new String[]{"Inglaterra", "España", "Alemania", "Francia"}, 0));
+        questions.add(new Question("¿Qué equipo ha ganado la Copa del Mundo más veces?", new String[]{"Alemania", "Argentina", "Brasil", "Italia"}, 2));
+        questions.add(new Question("¿Qué Copa de Europa ganó el Real Madrid gracias a la volea de Zidane?", new String[]{"Séptima", "Novena", "Octava", "Décima"}, 1));
+        questions.add(new Question("¿Quién ha ganado más Balones de Oro en toda la historia?", new String[]{"Messi", "Cristiano Ronaldo", "Ronaldo Nazario", "Maradona"}, 0));
+        questions.add(new Question("¿Cuál es el equipo de fútbol más antiguo del mundo?", new String[]{"Manchester United FC", "Sheffield FC", "Sevilla FC", "CA Boca Juniors "}, 1));
 
-        // Agregar más preguntas según sea necesario
 
         showQuestionRadioButton();
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                // Manejar el evento de selección/deselección de los RadioButtons aquí
+                RadioButton selectedRadioButton = findViewById(checkedId);
+                if (selectedRadioButton != null) {
+                    String selectedText = selectedRadioButton.getText().toString();
+                    // Puedes hacer algo con el texto seleccionado si es necesario
+                }
+            }
+        });
 
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
                 checkAnswer();
                 currentQuestionIndex++;
                 if (currentQuestionIndex < questions.size()) {
+
                     showQuestionRadioButton();
                 } else {
                     // Juego terminado, mostrar puntuación
@@ -59,45 +73,35 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void showQuestionRadioButton() {
+        radioGroup.clearCheck(); // Deselecciona cualquier RadioButton previamente seleccionado
         Question question = questions.get(currentQuestionIndex);
         questionTextView.setText(question.getQuestion());
         String[] options = question.getOptions();
-        option1RadioButton.setText(options[0]);
-        option2RadioButton.setText(options[1]);
-        option3RadioButton.setText(options[2]);
-        option4RadioButton.setText(options[3]);
+
+        // Elimina los RadioButton existentes antes de agregar nuevos
+        radioGroup.removeAllViews();
+
+        for (int i = 0; i < options.length; i++) {
+            RadioButton radioButton = new RadioButton(this);
+            radioButton.setText(options[i]);
+            radioGroup.addView(radioButton);
+        }
     }
 
     private void checkAnswer() {
         Question question = questions.get(currentQuestionIndex);
         int correctAnswerIndex = question.getCorrectAnswerIndex();
-        int selectedAnswerIndex = getCheckedRadioButtonIndex();
+        int selectedAnswerIndex = radioGroup.indexOfChild(findViewById(radioGroup.getCheckedRadioButtonId()));
 
         if (selectedAnswerIndex == correctAnswerIndex) {
             // Respuesta correcta
             score += 3;
-            Toast.makeText(this, "Puntuación: "+score, Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "¡Correcto! Puntuación: " + score, Toast.LENGTH_SHORT).show();
         } else {
             // Respuesta incorrecta
             score -= 2;
-            Toast.makeText(this, "Incorrecto. Puntuación: "+score, Toast.LENGTH_SHORT).show();
+            againButton.setVisibility(View.VISIBLE);
+            Toast.makeText(this, "¡Incorrecto! Puntuación: " + score, Toast.LENGTH_SHORT).show();
         }
     }
-
-    private int getCheckedRadioButtonIndex() {
-        if (option1RadioButton.isChecked()) {
-            return 0;
-        } else if (option2RadioButton.isChecked()) {
-            return 1;
-        } else if (option3RadioButton.isChecked()) {
-            return 2;
-        } else if (option4RadioButton.isChecked()) {
-            return 3;
-        }
-        return -1; // Ninguna opción seleccionada
-    }
-
-
-
-
 }
