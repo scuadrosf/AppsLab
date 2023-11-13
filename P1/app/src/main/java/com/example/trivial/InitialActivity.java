feb2d81f03dea99028217443a7856d17060c8cc6
scuadrosf <101071401+scuadrosf@users.noreply.github.com>
2023-11-13 21:08:19 +0100

    help

--- P1/app/src/main/java/com/example/trivial/InitialActivity.java
+++ P1/app/src/main/java/com/example/trivial/InitialActivity.java
@@ -6,16 +6,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class InitialActivity extends AppCompatActivity {
 
     Button start;
+    ImageButton helpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial_layout);
         start = findViewById(R.id.buttonIniciar);
+        helpButton = findViewById(R.id.helpButton);
+
+        helpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InitialActivity.this, UserManualActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
     public void startTrivial(View view){
         Intent intent = new Intent(this, MainActivity.class);
