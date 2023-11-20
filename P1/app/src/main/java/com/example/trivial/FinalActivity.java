9a6ede43d744c07310ea0a97222b12f971d2d73c
scuadrosf <101071401+scuadrosf@users.noreply.github.com>
2023-11-20 20:07:36 +0100

    añadido las animaciones de buena puntuacion o mala

--- P1/app/src/main/java/com/example/trivial/FinalActivity.java
+++ P1/app/src/main/java/com/example/trivial/FinalActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import com.airbnb.lottie.LottieAnimationView;
 
 public class FinalActivity extends AppCompatActivity {
 
@@ -18,23 +19,41 @@ public class FinalActivity extends AppCompatActivity {
         setContentView(R.layout.activity_final);
 
         Intent intent = getIntent();
-        if (intent.hasExtra("SCORE_FINAL")){
+        if (intent.hasExtra("SCORE_FINAL")) {
             finalScore = intent.getIntExtra("SCORE_FINAL", 0);
         }
 
         TextView scoreTextView = findViewById(R.id.scoreTextView);
-        String showPuntutation = "Puntuación: " + finalScore;
-        scoreTextView.setText(showPuntutation);
+        String showPunctuation = "Puntuación: " + finalScore;
+        scoreTextView.setText(showPunctuation);
+
+        LottieAnimationView successAnimation = findViewById(R.id.animation_view);
+        LottieAnimationView failureAnimation = findViewById(R.id.animation_failure);
 
         Button backButton = findViewById(R.id.backButton);
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(FinalActivity.this, MainActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // Cierra todas las actividades anteriores
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(intent);
-                finish(); // Finaliza la actividad actual
+                finish();
             }
         });
+
+        // Mostrar animación según la puntuación
+        if (finalScore >= 8) {
+            // Mostrar animación de éxito y ocultar la de fallo
+            successAnimation.setVisibility(View.VISIBLE);
+            successAnimation.playAnimation();
+
+            failureAnimation.setVisibility(View.GONE);
+        } else {
+            // Mostrar animación de fallo y ocultar la de éxito
+            failureAnimation.setVisibility(View.VISIBLE);
+            failureAnimation.playAnimation();
+
+            successAnimation.setVisibility(View.GONE);
+        }
     }
 }
