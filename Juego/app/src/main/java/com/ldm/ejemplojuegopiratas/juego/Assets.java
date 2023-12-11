774066ff541443bd95b5682032abc434e7e6ba64
scuadrosf <101071401+scuadrosf@users.noreply.github.com>
2023-12-11 22:01:32 +0100

    Imagenes "snake"

---
+++ Juego/app/src/main/java/com/ldm/ejemplojuegopiratas/juego/Assets.java
@@ -0,0 +1,31 @@
+package com.ldm.ejemplojuegopiratas.juego;
+
+import com.ldm.ejemplojuegopiratas.Pixmap;
+import com.ldm.ejemplojuegopiratas.Sonido;
+
+public class Assets {
+    public static Pixmap fondo;
+    public static Pixmap logo;
+    public static Pixmap menuprincipal;
+    public static Pixmap botones;
+    public static Pixmap ayuda1;
+    public static Pixmap ayuda2;
+    public static Pixmap ayuda3;
+    public static Pixmap numeros;
+    public static Pixmap preparado;
+    public static Pixmap menupausa;
+    public static Pixmap finjuego;
+    public static Pixmap futArriba;
+    public static Pixmap futIzquierda;
+    public static Pixmap futAbajo;
+    public static Pixmap futDerecha;
+    public static Pixmap goles;
+    public static Pixmap premio1;
+    public static Pixmap premio2;
+    public static Pixmap premio3;
+
+    public static Sonido pulsar;
+    public static Sonido ataque;
+    public static Sonido derrota;
+
+}
---
+++ Juego/app/src/main/java/com/ldm/ejemplojuegopiratas/juego/LoadingScreen.java
@@ -0,0 +1,64 @@
+package com.ldm.ejemplojuegopiratas.juego;
+
+import com.ldm.ejemplojuegopiratas.Juego;
+import com.ldm.ejemplojuegopiratas.Graficos;
+import com.ldm.ejemplojuegopiratas.Pantalla;
+import com.ldm.ejemplojuegopiratas.Graficos.PixmapFormat;
+
+public class LoadingScreen extends Pantalla{
+    public LoadingScreen(Juego juego) {
+        super(juego);
+    }
+
+    @Override
+    public void update(float deltaTime) {
+        Graficos g = juego.getGraphics();
+        Assets.fondo = g.newPixmap("fondo.png", PixmapFormat.RGB565);
+        Assets.logo = g.newPixmap("logo.png", PixmapFormat.ARGB4444);
+        Assets.menuprincipal = g.newPixmap("menuprincipal.png", PixmapFormat.ARGB4444);
+        Assets.botones = g.newPixmap("botones.png", PixmapFormat.ARGB4444);
+        Assets.ayuda1 = g.newPixmap("ayuda1.png", PixmapFormat.ARGB4444);
+        Assets.ayuda2 = g.newPixmap("ayuda2.png", PixmapFormat.ARGB4444);
+        Assets.ayuda3 = g.newPixmap("ayuda3.png", PixmapFormat.ARGB4444);
+        Assets.numeros = g.newPixmap("numeros.png", PixmapFormat.ARGB4444);
+        Assets.preparado = g.newPixmap("preparado.png", PixmapFormat.ARGB4444);
+        Assets.menupausa = g.newPixmap("menupausa.png", PixmapFormat.ARGB4444);
+        Assets.finjuego = g.newPixmap("finjuego.png", PixmapFormat.ARGB4444);
+        Assets.futArriba = g.newPixmap("futArriba.png", PixmapFormat.ARGB4444);
+        Assets.futIzquierda = g.newPixmap("futIzq.png", PixmapFormat.ARGB4444);
+        Assets.futAbajo = g.newPixmap("futAbajo.png", PixmapFormat.ARGB4444);
+        Assets.futDerecha = g.newPixmap("futDrch.png", PixmapFormat.ARGB4444);
+        Assets.goles = g.newPixmap("golazo.png", PixmapFormat.ARGB4444);
+        Assets.premio1 = g.newPixmap("premio1.png", PixmapFormat.ARGB4444);
+        Assets.premio2 = g.newPixmap("premio2.png", PixmapFormat.ARGB4444);
+        Assets.premio3 = g.newPixmap("premio3.png", PixmapFormat.ARGB4444);
+        Assets.pulsar = juego.getAudio().nuevoSonido("pulsar.ogg");
+        Assets.ataque = juego.getAudio().nuevoSonido("ataque.ogg");
+        Assets.derrota = juego.getAudio().nuevoSonido("derrota.ogg");
+
+
+        Configuraciones.cargar(juego.getFileIO());
+        juego.setScreen(new MainMenuScreen(juego));
+    }
+
+    @Override
+    public void present(float deltaTime) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
