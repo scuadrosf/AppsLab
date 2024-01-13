7911046574463d047a42477b2e8c2b4bae6ccc7e
scuadrosf <101071401+scuadrosf@users.noreply.github.com>
2024-01-13 19:38:07 +0100

    help

--- P4/app/src/main/java/com/example/mindrelax/Help.java
+++ P4/app/src/main/java/com/example/mindrelax/Help.java
@@ -1,8 +1,10 @@
 package com.example.mindrelax;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Help extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class Help extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        TextView helpTextView = findViewById(R.id.helpText);
+        Button closeHelpButton = findViewById(R.id.closeHelpButton);
+
+        String helpText = getString(R.string.contenido_help); // Define un string largo en strings.xml
+        helpTextView.setTextColor(getColor(R.color.black));
+        helpTextView.setTextSize(18);
+        helpTextView.setText(helpText);
+
+        closeHelpButton.setOnClickListener(v -> finish());
     }
 }
--- P4/app/src/main/java/com/example/mindrelax/Respiracion.java
+++ P4/app/src/main/java/com/example/mindrelax/Respiracion.java
@@ -1,7 +1,6 @@
 package com.example.mindrelax;
 
 import android.content.Intent;
-import android.media.Image;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
