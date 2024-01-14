6f9e6277c6da5b1a518d4e3d343b65ea1d0907ee
scuadrosf <101071401+scuadrosf@users.noreply.github.com>
2024-01-14 15:41:55 +0100

    done

--- P4/app/src/main/java/com/example/mindrelax/Profile.java
+++ P4/app/src/main/java/com/example/mindrelax/Profile.java
@@ -1,10 +1,12 @@
 package com.example.mindrelax;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,6 +21,7 @@ public class Profile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) ImageButton backButton = findViewById(R.id.btnBack);
         newPasswordEditText = findViewById(R.id.newPasswordEditText);
         TextView usernameTextView = findViewById(R.id.usernameTextView);
         Button changePasswordButton = findViewById(R.id.changePasswordButton);
@@ -29,6 +32,11 @@ public class Profile extends AppCompatActivity {
 
         changePasswordButton.setOnClickListener(v -> {
             changePassword();
+            Intent intent = new Intent(Profile.this, MainActivity.class);
+            startActivity(intent);
+        });
+
+        backButton.setOnClickListener(v -> {
             Intent intent = new Intent(Profile.this, MainActivity.class);
             startActivity(intent);
         });
